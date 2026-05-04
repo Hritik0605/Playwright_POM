fb5e2beff5256055d6dfa8cf35b511c42a458124
Hritik0605 <hritik.choudhary@gammaedge.io>
2026-05-04 14:52:06 +0530

    first commit

---
+++ tests/demo/login.spec.js
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/login';
+import { log } from 'node:console';
+
+test('login test', async ({ page }) => {
+ 
+  const loginPage = new LoginPage(page);
+  await loginPage.navigate(); // Navigate to the login page
+  log('Navigated to login page');
+
+  
+  await loginPage.login('testuser', 'SuperSecretPassword!');
+
+  // Assert successful login (replace with actual assertion, e.g., check for dashboard element)
+ // await expect(page).toHaveURL(); // Example assertion
+  await page.waitForTimeout(2000);
+   // Wait for 2 seconds to observe the result (optional)
+});
