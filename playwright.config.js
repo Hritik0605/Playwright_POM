edbf82b4bc0ba41d5e60b52a3ef54560309cd7a4
Hritik0605 <hritik.choudhary@gammaedge.io>
2026-05-04 17:15:50 +0530

    Thrid commit

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import { junit } from 'node:test/reporters';
 
 /**
  * Read environment variables from file.
@@ -23,7 +24,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter:[ ['html'],['junit',{
+    outputFile: 'test-results.xml',
+  }] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
