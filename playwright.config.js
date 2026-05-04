c2c84aa1dc9eee266b57e96000bc6a04431e62ff
Hritik0605 <hritik.choudhary@gammaedge.io>
2026-05-04 18:16:11 +0530

    fourth commit

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:[ ['html'],['junit',{
     outputFile: 'test-results.xml',
-  }] ],
+  }],['Allure',{outputDir: 'allure-results'}] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
